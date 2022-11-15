e20d1ddbd01418ab567f773acac8117c88a6f805
adamY3geR <adamyeger@gmail.com>
2022-11-15 23:29:48 +0200

    nnn

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,4 @@
 package com.company;
-import com.company.Node;
 
 public class Main {
     public static void main(String[] args) {
@@ -7,11 +6,23 @@ public class Main {
                 new Node<Integer>(18,
                         new Node<Integer>(4,
                                 new Node<Integer>(19))));
-        int x =19;
-        System.out.println("there are "+numOfNums(chain)+" numbers in the chain"); //Q1
-        System.out.println("the int x is in the chain? "+isNumInLst(x,chain)); //Q2
-        System.out.println("the num x appear in the chain "+numOfAppearances(chain,x)+" times");
-
+        Node<Integer> L = new Node<Integer>(4,
+                new Node<Integer>(4,
+                        new Node<Integer>(3,
+                                new Node<Integer>(3,
+                                        new Node<Integer>(3,
+                                                new Node<Integer>(2,
+                                                        new Node<Integer>(1,
+                                                                new Node<Integer>(2,
+                                                                        new Node<Integer>(4,
+                                                                                new Node<Integer>(19,
+                                                                                        new Node<Integer>(5)))))))))));
+        int x = 2;
+        System.out.println("there are "+numOfNums(chain)+" numbers in the Node chain"); //Q1
+        System.out.println("the int x is in the Node chain? "+isNumInLst(x,chain)); //Q2
+        System.out.println("the num x appear in the Node chain "+numOfAppearances(chain,x)+" times");
+        int[] arr1 = isXListChecker(L,x);
+        System.out.println("Is the Node L is Xlist? "+isXList(arr1, x));
     }
 
     /**
@@ -58,5 +69,42 @@ public class Main {
             chain = chain.getNext();
         }
         return num;
+    }
+
+    /**
+     * the method make an array that helps find if  it's a Xlist
+     * @param L Node
+     * @param x Integer
+     * @return Integer for isXlist
+     */
+    public static int[] isXListChecker(Node<Integer> L, int x) {
+        int[] arr = new int[x];
+            for (int i = 1; i <= arr.length; i++) {
+                if (L.getValue() > x) {
+                    L = L.getNext();
+                    i = 1;
+                }
+                if (L.getValue() == (i)) {
+                    arr[i-1] = arr[i-1] + 1;
+                    L = L.getNext();
+                    i = 1;
+                }
+            }
+        return arr;
+    }
+    /**
+     * the method get the value of isXListChecker and check if its true of false
+     * @return boolean
+     */
+    public static boolean isXList(int []arr1, int x){
+        int count = 0;
+        for (int i = 0; i <= arr1.length; i++) {
+            if (arr1[i] == i+1)
+                count++;
+        }
+        if ((count)%x == 0 && count > 0)
+            return true;
+        else
+            return false;
     }
 }
